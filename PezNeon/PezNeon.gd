0d98175eea9286a897ac8725bff13e3fa2e994e7
giulianubile <giulianubile98@gmail.com>
2022-11-01 16:43:54 +0100

    mundolisto

--- PezNeon/PezNeon.gd
+++ PezNeon/PezNeon.gd
@@ -1,55 +1,50 @@
 extends Area2D
+var movement = Vector2(1,1)
+const SPEED = 100
+
+var limits_DS = Vector2(1400,-5300)
+var limits_II = Vector2(0,1660)
+
+var velocity = Vector2(0,0)
 
 
-export (int) var speed = 100
-export (Vector2) var limits = Vector2(1480,600)
-var movement = Vector2(1,0)
-var movleft = false
 
-
-# Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		$AnimationPlayer.play("nadar")		
-		self.position += movement.normalized() * speed * delta
-		
-		if(self.position.x <= 0) || (self.position.x >= limits.x):
-			self.position.x = clamp(self.position.x, 0, limits.x)
-			
-		
-		if Input.is_action_pressed("ui_right"):
-			self.rotation_degrees = 0	
-			movement = Vector2(1,0)
-			$Sprite.set_flip_h(false)
-			movleft = false
-			
-		elif Input.is_action_pressed("ui_left"):
-			self.rotation_degrees = 0
-			movement = Vector2(-1,0)
-			$Sprite.set_flip_h(true)
-			movleft = true
+	$AnimationPlayer.play("nadar")
+	
+	if Input.is_action_pressed("ui_up") and self.position.y > limits_DS.y:
+		if $Sprite.flip_h == true:
+			$Sprite.rotation_degrees = 45
+		else:
+			$Sprite.rotation_degrees = -45
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down") and self.position.y < limits_II.y:
+		if $Sprite.flip_h == true:
+			$Sprite.rotation_degrees = -45
+		else:
+			$Sprite.rotation_degrees = 45
+		velocity.y = SPEED
+	else:
+		$Sprite.rotation_degrees = 0
+	if Input.is_action_pressed("ui_right") and self.position.x < limits_DS.x:
+		velocity.x = SPEED
+		if $Sprite.flip_h == true:
+			$Sprite.flip_h = false
+		#$AudioStreamPlayer2D.play()
+		velocity.x = velocity.x + SPEED
+	elif Input.is_action_pressed("ui_left") and self.position.x > limits_II.x:
+		$Sprite.flip_h = true
+		velocity.x = -SPEED
+		#$AudioStreamPlayer2D.play()
+		velocity.x = velocity.x - SPEED
+	self.position += movement.normalized() * velocity * delta
+	velocity = Vector2(0,0)
 
-		elif Input.is_action_pressed("ui_up"):
-			if(movleft):
-				self.rotation_degrees = 45
-				$Sprite.set_flip_h(true)
-				movement = Vector2(-1,-1)
-			else:				
-				self.rotation_degrees = -45
-				$Sprite.set_flip_h(false)
-				movement = Vector2(1,-1)
-			
-			
-		elif Input.is_action_pressed("ui_down"):
-			if(movleft):
-				self.rotation_degrees = -45
-				$Sprite.set_flip_h(true)
-				movement = Vector2(-1,1)
-			else:
-				self.rotation_degrees = 45
-				$Sprite.set_flip_h(false)
-				movement = Vector2(1,1)
-				
-				
+
+
+
+
+
 
