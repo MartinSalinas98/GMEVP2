56a77acfe77a0fb5bfc65e6cbc34170b0678ddd3
MartinSalinas98 <ssalinasmartin@gmail.com>
2022-11-02 22:49:20 +0100

    Added opening sound

--- scripts/Ostra.gd
+++ scripts/Ostra.gd
@@ -2,4 +2,5 @@ extends Area2D
 
 # Function that will be entered when a fish enters in contact with the oyster
 func _on_Ostra_body_entered(body):
-	$AnimationPlayer.play("open")
+	$Animation.play("open")
+	$OpeningSound.play()
