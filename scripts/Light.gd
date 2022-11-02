660109ddbcd137e921bed45ccece2db201d2e2f0
giulianubile <giulianubile98@gmail.com>
2022-11-03 00:55:36 +0100

    luz

--- scripts/Light.gd
+++ scripts/Light.gd
@@ -4,12 +4,13 @@ var on = false
 
 func _on_BotonLuz_body_entered(body):
 	self.enabled = !self.enabled
-	$LightSwitch/ClickSound.play()
 	
 	if on == false:
 		$LightSwitch/Animation.play("on")
+		$AnimationPlayerLight.play("on")
 		on = true
 	else:
 		$LightSwitch/Animation.play("off")
+		$AnimationPlayerLight.play("off")
 		on = false
 
